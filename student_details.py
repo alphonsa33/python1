6408add8377a570683e841120befb655ec6c5076
alphonsa33 <alphonsathresiajacob2028@cs.sjcetpalai.ac.in>
2024-10-01 15:37:43 +0530

    document header added

---
+++ student_details.py
@@ -0,0 +1,15 @@
+#Author: Alph
+#Date:01-10-2024
+#Python program to get the student details
+#version:1.0
+name=input("Enter your name:")
+roll_number=int(input("Enter your roll number:"))
+roll_number=roll_number+1
+print("name of the student:",name)
+print("roll_number:",roll_number)
+cgpa=float(input("enter your cgpa:"))
+percentage_marks=cgpa*10
+print("percentage_marks:",percentage_marks,"%")
+
+
+
