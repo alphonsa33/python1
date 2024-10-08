499e5749fe0d872540841045ad916ffc436df44d
alphonsa33 <alphonsathresiajacob2028@cs.sjcetpalai.ac.in>
2024-10-08 15:41:31 +0530

    Add files via upload

---
+++ alphinputnum.py
@@ -0,0 +1,8 @@
+'''
+author=Alphonsa
+version=1.0
+Program to perform operations
+'''
+num_1=int(input("Enter the first number:"))
+num_2=int(input("Enter the second number:"))
+num_3=int(input("Enter the third number:"))
