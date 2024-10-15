50dcbc2a98df309ad6280a22d6487b2787fbc2a9
alphonsa33 <alphonsathresiajacob2028@cs.sjcetpalai.ac.in>
2024-10-15 15:38:08 +0530

    Add files via upload

---
+++ alphsimpleinterest.py
@@ -0,0 +1,9 @@
+'''
+Python program to find simple interest
+author-alph
+'''
+principle=int(input("enter principle"))
+rate=int(input("enter rate"))
+year=int(input("enter no of year"))
+simple_interest=principle*rate*year
+print(simple_interest)
