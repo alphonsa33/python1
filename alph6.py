da29f9eda973d19ec499e582b25f2b72e4a5fed6
alphonsa33 <alphonsathresiajacob2028@cs.sjcetpalai.ac.in>
2024-10-15 14:39:31 +0530

    Add files via upload

---
+++ alph6.py
@@ -0,0 +1,11 @@
+'''
+Python program to chech whether the given number is positive or not
+author-alph
+'''
+age=int(input("enter the age"))
+if age<10:
+    print("Fare is 7")
+elif age>=10 and age<60:
+    print("Fare is 10")
+else:
+    print("fare is 5")
