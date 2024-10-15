97b429e6d379379cec0db1c4c97e3e261a0e0b37
alphonsa33 <alphonsathresiajacob2028@cs.sjcetpalai.ac.in>
2024-10-15 14:45:59 +0530

    Add files via upload

---
+++ alphsmallestof2.py
@@ -0,0 +1,10 @@
+'''
+Python program to find the larger of two numbers
+author-alph
+'''
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+if num1>num2:
+    print("num1 is greater")
+else:
+    print("num2 is greater")
