0989cac8a9f9d4e5bf90991464027c2e0c793913
alphonsa33 <alphonsathresiajacob2028@cs.sjcetpalai.ac.in>
2024-10-15 15:58:15 +0530

    Add files via upload

---
+++ alph8.py
@@ -0,0 +1,13 @@
+'''
+Python program to generate n odd numbers
+author-alph
+'''
+limit=int(input("Enter the limit:"))
+odd_number=1
+count=0
+while count<limit:
+    print(odd_number,"\t",end=" ")
+    count+=1
+    odd_number+=2
+
+
