52daf816190068cb1050871e03b1ca63869a6677
alphonsa33 <alphonsathresiajacob2028@cs.sjcetpalai.ac.in>
2024-10-15 14:07:18 +0530

    Add files via upload

---
+++ alphpositive.py
@@ -0,0 +1,9 @@
+'''
+Python program to chech whether the given number is positive or not
+author-alph
+'''
+number=int(input("Enter a number:"))
+if number>0:
+     print("the given number:",number,"is positive")
+else:
+    print("the given number:",number,"is negative")
