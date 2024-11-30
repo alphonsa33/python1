421667d7d81c69e3c3c67086351556752b759ccc
alphonsa33 <alphonsathresiajacob2028@cs.sjcetpalai.ac.in>
2024-11-30 10:35:21 +0530

    Add files via upload

---
+++ alphtriangle.py
@@ -0,0 +1,10 @@
+def triangle():
+    base=int(input("Enter the length of base"))
+    alt=int(input("Enter the length of altitude"))
+    hyp=int(input("Enter the length of hyp side"))
+    if hyp*hyp==(base*base)+(alt*alt):
+        print("It is a right triangle")
+    else:
+        print("It is not a right triangle")
+triangle()
+
